01afbe8775d006b7de0e57e0fe868f7166e18c7d
christianvn15-ui <christianvn15@yahoo.com>
2025-10-28 10:01:09 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function home() {
+  window.location.href = "homepage.html";
+}
+
+function info() {
+  window.location.href = "information.html";
+}
+
+function business() {
+  window.location.href = "company.html";
+}
